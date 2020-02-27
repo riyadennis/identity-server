5548a1f19c9d9c190c5f7370bfcf9636da7afcb5
Riya Dennis <riyacv@gmail.com>
2020-02-27 19:14:08 +0000

    added test and prod environment

--- main.go
+++ main.go
@@ -2,17 +2,19 @@ package main
 
 import (
 	"flag"
+	"os"
 
 	"github.com/riyadennis/identity-server/internal"
 	"github.com/riyadennis/identity-server/internal/handlers"
 )
 
 var (
-	port = flag.String("port", ":8095", "port http server will listen to")
+	port = flag.String("port", ":8095",
+		"port http server will listen to")
 )
 
 func main() {
 	flag.Parse()
-	handlers.Init()
+	handlers.Init(os.Getenv("ENV"))
 	internal.Server(*port)
 }
