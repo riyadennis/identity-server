89fbf8d2991580d2a5f55f639bac23eabaa0e663
Riya Dennis <riyacv@gmail.com>
2020-03-11 17:38:46 +0000

    use env vars for sensitive data

--- MySQL/db.sh
+++ MySQL/db.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 
+source .env
 docker network create backend
 
 docker build -t mysql-development .
 
 docker run --rm --name=mysql-development \
--e MYSQL_USER='root' -e MYSQL_ROOT_PASSWORD='root' \
--e MYSQL_DATABASE='identity_db' -p=3308:3306 \
+-e MYSQL_USER='{$MYSQL_USER}' -e MYSQL_ROOT_PASSWORD='{$MYSQL_ROOT_PASSWORD}' \
+-e MYSQL_DATABASE='{$MYSQL_DATABASE}' -p=3308:3306 \
 --network=backend mysql-development
 
 docker network connect --alias=mysql-development backend mysql-development
