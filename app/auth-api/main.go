d0bfdfbbedd20c1ee26819128060f95e8523ca46
Riya Dennis <riyacv@gmail.com>
2021-06-17 16:17:26 +0100

    refactor(main): removed init and added initialised logger

--- app/auth-api/main.go
+++ app/auth-api/main.go
@@ -4,48 +4,29 @@ import (
 	"log"
 	"os"
 
+	"github.com/joho/godotenv"
+
 	// initialise mysql driver
 	_ "github.com/go-sql-driver/mysql"
 	// initialise migration settings
 	_ "github.com/golang-migrate/migrate/source/file"
 
-	"github.com/joho/godotenv"
-	"github.com/sirupsen/logrus"
-
 	"github.com/riyadennis/identity-server/app/auth-api/handlers"
 	"github.com/riyadennis/identity-server/business/store"
 )
 
-func init() {
-	err := godotenv.Load()
-	if err != nil {
-		logrus.Fatalf("failed to open env file: %v", err)
-	}
-
-	if os.Getenv("PORT") == "" {
-		logrus.Fatal("invalid setting no port number")
-	}
-
-	if os.Getenv("ENV") == "" {
-		logrus.Fatal("invalid setting no environment")
-	}
-
-	if os.Getenv("ENV") != "test" {
-		file, err := os.Create("identity.log")
-		if err != nil {
-			logrus.Fatalf("failed to create log file: %v", err)
-		}
-
-		logrus.SetOutput(file)
-	}
-}
-
 func main() {
 	cfg := store.NewENVConfig()
+	logger := log.New(os.Stdout, "IDENTITY : ", log.LstdFlags|log.Lmicroseconds|log.Lshortfile)
+
+	err := godotenv.Load()
+	if err != nil {
+		logger.Fatalf("failed to open env file: %v", err)
+	}
 
 	db, err := store.Connect(cfg.DB)
 	if err != nil {
-		logrus.Errorf("failed to connect to database: %v", err)
+		logger.Fatalf("failed to connect to database: %v", err)
 	}
 
 	defer func() {
@@ -54,13 +35,11 @@ func main() {
 
 	err = store.Migrate(db, cfg.DB.Database, cfg.BasePath)
 	if err != nil {
-		logrus.Errorf("migration failed: %v", err)
+		logger.Panicf("migration failed: %v", err)
 	}
-
-	logger := log.New(os.Stdout, "IDENTITY : ", log.LstdFlags|log.Lmicroseconds|log.Lshortfile)
 
 	err = handlers.NewServer(os.Getenv("PORT")).Run(db, cfg.Token, logger)
 	if err != nil {
-		logrus.Errorf("error running server: %v", err)
+		logger.Panicf("error running server: %v", err)
 	}
 }
