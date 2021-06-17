7fb948de2252a8a27680dcdfb20b76004f175cac
Riya Dennis <riyacv@gmail.com>
2021-06-17 16:18:19 +0100

    refactor(routes): moved routing closer to end point declaration to improve readability.

--- app/auth-api/handlers/endpoints.go
+++ app/auth-api/handlers/endpoints.go
@@ -1,5 +1,15 @@
 package handlers
 
+import (
+	"database/sql"
+	"log"
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
+	"github.com/riyadennis/identity-server/business/store"
+	"github.com/riyadennis/identity-server/foundation/middleware"
+)
+
 const (
 	// RegisterEndpoint is to create a new user
 	RegisterEndpoint = "/register"
@@ -14,3 +24,17 @@ const (
 	// logged in user with valid token can access
 	HomeEndPoint = "/home"
 )
+
+func loadRoutes(conn *sql.DB, tc *store.TokenConfig, logger *log.Logger) http.Handler {
+	h := NewHandler(store.NewDB(conn), tc, logger)
+	router := httprouter.New()
+	allowedOrigins := []string{"*"}
+
+	// register routes here
+	router.POST(RegisterEndpoint, middleware.CORS(h.Register, allowedOrigins))
+	router.POST(LoginEndPoint, middleware.CORS(h.Login, allowedOrigins))
+	router.DELETE(DeleteEndpoint, middleware.CORS(middleware.Auth(h.Delete, tc, logger), allowedOrigins))
+	router.GET(HomeEndPoint, middleware.CORS(middleware.Auth(Home, tc, logger), allowedOrigins))
+
+	return router
+}
