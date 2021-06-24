5b36b318a9959e53a276fcaa7bdc9d8a02501a08
Riya Dennis <riyacv@gmail.com>
2021-06-24 10:19:15 +0100

    chore(probes): more info to liveness end point

--- app/auth-api/handlers/probes.go
+++ app/auth-api/handlers/probes.go
@@ -2,15 +2,41 @@ package handlers
 
 import (
 	"database/sql"
+	"encoding/json"
 	"net/http"
+	"os"
 
 	"github.com/julienschmidt/httprouter"
 	"github.com/riyadennis/identity-server/foundation"
 )
 
-// Liveness returns liveness status of the service
+// Liveness returns liveness status of the service,
+// if app is deployed in kubernetes cluster it will return pod, node and namespace.
+// the env vars are to be set from the manifest.
 func Liveness(w http.ResponseWriter, _ *http.Request, _ httprouter.Params) {
-	_ = foundation.JSONResponse(w, http.StatusOK, "OK", "")
+	hostName, err := os.Hostname()
+	if err != nil {
+		hostName = "unavailable"
+	}
+	data := struct {
+		Status    string `jsonapi:"status,omitempty"`
+		Host      string `jsonapi:"host,omitempty"`
+		Pod       string `jsonapi:"pod,omitempty"`
+		PodIP     string `jsonapi:"podIP,omitempty"`
+		Node      string `jsonapi:"node,omitempty"`
+		Namespace string `jsonapi:"namespace,omitempty"`
+	}{
+		Status:    "up",
+		Host:      hostName,
+		Pod:       os.Getenv("KUBERNETES_PODNAME"),
+		PodIP:     os.Getenv("KUBERNETES_NAMESPACE_POD_IP"),
+		Node:      os.Getenv("KUBERNETES_NODENAME"),
+		Namespace: os.Getenv("KUBERNETES_NAMESPACE"),
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+
+	_ = json.NewEncoder(w).Encode(data)
 }
 
 // Ready returns readiness status of the service
